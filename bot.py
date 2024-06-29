18f7815b44d4c6c28fb6954fb2748cf1ff3e8744
Falbue <cyansair05@gmail.com>
2024-06-29 16:00:57 +0500

    Библиотеки

--- bot.py
+++ bot.py
@@ -2,3 +2,8 @@ bot_api = ''
 folder = '/data/data/com.termux/files/home'
 id_admin = 1
 # -----------------------------------------------------------
+import telebot
+import subprocess
+import os
+import sys
+import time
