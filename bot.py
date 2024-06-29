a865988b75ff7d080a68482ff5d4394923e14f96
Falbue <cyansair05@gmail.com>
2024-06-29 16:05:19 +0500

    Начало работы с ботом

--- bot.py
+++ bot.py
@@ -6,4 +6,16 @@ import telebot
 import subprocess
 import os
 import sys
-import timebot = telebot.TeleBot(API)
+import time
+
+bot = telebot.TeleBot(API)
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    if message.from_user.id == ALLOWED_USER_ID:
+        bot.send_message(message.chat.id, "Привет!")
+    else:
+        bot.send_message(message.chat.id, "У вас нет прав для использования этого бота.")
+
+bot.send_message(ADMIN, "Бот запущен...")
+bot.polling()
